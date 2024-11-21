aa89555c14a63b1dc1f51f7a3e94c1310d558f78
Mritunjay Namdeo <namdeomritunjay@gmail.com>
2024-11-21 22:38:32 +0530

    Rearranged the files in c for c language and test folder for test files

---
+++ c/question67.c
@@ -0,0 +1,17 @@
+/*
+Create a function/method that takes, a word and returns true if the word has two consecutive identical letters.
+
+Input Format
+
+A string from the user
+
+Constraints
+
+No
+
+Output Format
+
+true or false
+
+*/
+
